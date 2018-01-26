08cdf8b550ada6201a8fa68038adce1cb2990e8c
roblesisaac <irobles1030@gmail.com>
2018-01-25 17:07:22 -0800

    Update sites.js

--- models/sites.js
+++ models/sites.js
@@ -1,1 +1,10 @@
-//info
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+var siteSchema = new mongoose.Schema({
+    name: String,
+    userId: String,
+    url: { type: String, unique: true }
+});
+
+module.exports = restful.model('site', siteSchema);
