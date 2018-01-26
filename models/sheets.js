37f88e58376c1b95a54fc352b1c48d041389a676
roblesisaac <irobles1030@gmail.com>
2018-01-26 15:31:12 -0800

    Create sheets.js

---
+++ models/sheets.js
@@ -0,0 +1,31 @@
+var restful = require('node-restful');
+var mongoose = restful.mongoose;
+
+var user = {
+  username: String,
+  apps: [String]
+};
+
+var script = {
+  name: String,
+  txt: String
+}
+
+var sheetSchema = new mongoose.Schema({
+    "name" : String,
+    "link" : String,
+    "begin" : String,
+    "public" : String,
+    "scripts": [script],
+    "tmplts" : [script],
+    "users": [user],
+    "siteId": String
+});
+
+// sheetSchema.pre('save', function (next) {
+//     var userId = this.userId;
+//     if (this.url === '') this.url = this.name;
+//     next();
+// });
+
+module.exports = restful.model('sheets', sheetSchema);
