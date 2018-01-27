151b6fde91c19fb396c35bec7d173a90be46a18f
roblesisaac <irobles1030@gmail.com>
2018-01-26 17:32:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var lessMiddleware = require('less-middleware');
 var mongoose = require('mongoose');
 
 var index = require('./routes/index');
-var api = require('./routes/api');
+// var api = require('./routes/api');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use(api);
+//app.use(api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
