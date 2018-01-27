f16bcd6e88e2836de46cdc27ee299a09dcde3e81
roblesisaac <irobles1030@gmail.com>
2018-01-26 19:01:08 -0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ var models = ['sheets', 'users', 'sites'];
 
 for (var i = 0; i < models.length; i++) collections[models[i]] = require('../models/' + models[i]);
 
+router.use('/');
 collections.users.methods(['get', 'put', 'post', 'delete']);
 collections.users.register(router, 'users');
 
