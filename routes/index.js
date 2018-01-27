e1d04f713ab7df10ab368ecf3fcc2a3791562dd0
roblesisaac <irobles1030@gmail.com>
2018-01-26 18:21:22 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,20 @@ var router = express.Router();
 var fs = require('fs');
 var tmplts = {};
 
-
-
-/* GET home page. */
+/* GET tmplts. */
 router.get('/', function(req, res, next) {
   fs.readdir('views/partials/', function (err, data) {
     for (i=0; i<data.length; i++) {
-      tmplts[data[i].slice(0,-4)] = fs.readFileSync('views/partials/' + data[i], 'utf8')
+      tmplts[data[i].slice(0,-4)] = fs.readFileSync('views/partials/' + data[i], 'utf8');
     } 
-    res.render('index', { 
-      title: 'Express',
-      partials: {nav: 'partials/nav'},
-      tmplts: tmplts
-    });
+    res.end(tmplts);
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Express'
   });
 });
 
