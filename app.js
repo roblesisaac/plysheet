02a8955882bb29612513b408c2fe22df1ca17f51
roblesisaac <irobles1030@gmail.com>
2018-01-26 18:56:51 -0800

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use(api);
+app.use(api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
