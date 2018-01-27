62876b7cca8cecb1fc4fdfa74a82ad1349409420
roblesisaac <irobles1030@gmail.com>
2018-01-26 18:49:27 -0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,9 @@ var auth = require('./auth');
 var restful = require('node-restful');
 var mongoose = restful.mongoose;
 var collections = {};
-// var models = ['sheets', 'tmplts', 'sites'];
+var models = ['sheets', 'users', 'sites'];
 
-// for (var i = 0; i < models.length; i++) collections[models[i]] = require('../models/' + models[i]);
+for (var i = 0; i < models.length; i++) collections[models[i]] = require('../models/' + models[i]);
 
 
 
