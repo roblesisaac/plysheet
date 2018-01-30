cfb211dd14335eee912bdfa7a0468656bbcab508
roblesisaac <irobles1030@gmail.com>
2018-01-29 19:54:57 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,12 @@ router.get('/tmplts', function(req, res, next) {
   });
 });
 
+
+
+router.get('/test', function(req, res){
+  res.end("ply.sheet ['ply-login'] = {data: function {} {return {username: '', password: '', ply: ply};}, methods: {login: function .post ('/ users', {username: this.username, password: this.password}) .this (function (res) {console.log (res.data)}};}}, template: tmplts._login , watch: {'ply.name': function () {console.log ('changed to' + ply.name);}}} for {var sheet in ply.sheet} {var vue = ply.sheet [sheet] ; delete vue.name; Vue.component (sheet, vue);} ")
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
