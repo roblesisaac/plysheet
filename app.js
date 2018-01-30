6be308bb7adf82830238673818149bb3c356f5cf
roblesisaac <irobles1030@gmail.com>
2018-01-29 16:39:50 -0800

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('view engine', 'hjs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
